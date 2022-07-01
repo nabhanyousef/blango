6e69982f0543f90e3a0b6250e098da6085a0b897
NABHAN YOUSEF <nbhan.y7y@gmail.com>
2022-07-01 20:32:38 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -4,4 +4,5 @@ import blog.views
 urlpatterns = [
     # other patterns
     path("", blog.views.index)
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,5 @@
-def index(request):
-    return render(request, "blog/index.html")
+from django.shortcuts import render, get_object_or_404
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
